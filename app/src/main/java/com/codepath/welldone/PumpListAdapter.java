8f16174b1ee205663dd405e7d5fffbfc66fc9237
i0jupiter <bhashinee.1911+git@gmail.com>
2014-07-08 07:22:58 -0700

    Added fragment layout for pump list. Next on:  - Sort pumps by status and last updated  - Make only the image clickable rather than the entire row  - Add tabs for List/Map view.

--- app/src/main/java/com/codepath/welldone/PumpListAdapter.java
+++ app/src/main/java/com/codepath/welldone/PumpListAdapter.java
@@ -1,29 +1,69 @@
 package com.codepath.welldone;
 
 import android.content.Context;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.codepath.welldone.helper.DateTimeUtil;
 import com.codepath.welldone.model.Pump;
 
+/**
+ * Adapter to hold a list of pumps with their numbers, color-coded state, and
+ * (reverse geo-coded in future) location.
+ */
 public class PumpListAdapter extends ArrayAdapter<Pump> {
+
+    private ImageView ivPump;
+    private TextView tvLastUpdated;
+    private TextView tvLocation;
+    private TextView tvPumpName;
+
     public PumpListAdapter(Context context) {
         super(context, R.layout.row_pump);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+
         Pump pump = getItem(position);
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.row_pump, parent, false);
         }
-        TextView tvLocation = (TextView) convertView.findViewById(R.id.tvPumpLocation);
-        TextView tvStatus = (TextView) convertView.findViewById(R.id.tvPumpStatus);
-        tvLocation.setText(String.format("(%f, %f)", pump.getLocation().getLatitude(), pump.getLocation().getLongitude()));
-        tvStatus.setText(pump.getStatus());
+
+        ivPump = (ImageView) convertView.findViewById(R.id.ivPump);
+        tvLastUpdated = (TextView) convertView.findViewById(R.id.tvPumpLastUpdated);
+        tvLocation = (TextView) convertView.findViewById(R.id.tvPumpLocation);
+        tvPumpName = (TextView) convertView.findViewById(R.id.tvPumpName);
+
+        // The last updated date is wrt the local time zone.
+        tvLastUpdated.setText(DateTimeUtil.getFriendlyLocalDateTime(pump.getUpdatedAt()));
+        tvPumpName.setText("Pump Name");
+        setPumpColorBasedOnStatus(pump.getStatus());
+
+        // XXX These should be reverse geo-coded to be human-readable
+        tvLocation.setText(String.format("(%f, %f)",
+                pump.getLocation().getLatitude(), pump.getLocation().getLongitude()));
         return convertView;
+    }
+
+    // Set the color of a pump number based on its work state
+    private void setPumpColorBasedOnStatus(String pumpStatus) {
+
+        // XXX Status should probably be an enum, in which case this would
+        // reduce to a switch case.
+        if (pumpStatus.equalsIgnoreCase("broken_permanent")) {
+            ivPump.setBackgroundColor(Color.RED);
+        } else if (pumpStatus.equalsIgnoreCase("fix_in_progress")) {
+            ivPump.setBackgroundColor(Color.YELLOW);
+        } else if (pumpStatus.equalsIgnoreCase("good")) {
+            ivPump.setBackgroundColor(Color.GREEN);
+        } else {
+            ivPump.setBackgroundColor(Color.DKGRAY);
+        }
     }
 }
---
+++ app/src/main/java/com/codepath/welldone/helper/DateTimeUtil.java
@@ -0,0 +1,28 @@
+package com.codepath.welldone.helper;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * A simple class to do basic Date/Time manipulations
+ */
+public class DateTimeUtil {
+
+    public static final String friendlyDateFormat = "yyyy-MM-dd HH:mm:ss";
+
+    /**
+     * Return timestamp in human-readable form wrt local timezone
+     *
+     * @param date to be parsed
+     * @return formatted timestamp
+     */
+    public static String getFriendlyLocalDateTime(Date date) {
+
+        final SimpleDateFormat sdf = new SimpleDateFormat(friendlyDateFormat);
+        sdf.setTimeZone(TimeZone.getDefault());
+        return sdf.format(date);
+    }
+
+}
