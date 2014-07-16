7e036163c6cea8f9a094e4caad60227200938a1e
Robert Karl <robertkarljr@gmail.com>
2014-07-15 22:54:13 -0700

    Styling. Removing unused files

--- app/src/main/java/com/codepath/welldone/activity/CreateReportActivity.java
+++ app/src/main/java/com/codepath/welldone/activity/CreateReportActivity.java
@@ -9,8 +9,6 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
@@ -63,25 +61,6 @@ public class CreateReportActivity extends Activity {
         Log.d("debug", "Working with pump: " + pumpToBeReported.getObjectId() + " " + pumpToBeReported.getName());
         setupViews();
         setupListeners();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.create_report, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
--- app/src/main/java/com/codepath/welldone/activity/DemoPushActivity.java
+++ app/src/main/java/com/codepath/welldone/activity/DemoPushActivity.java
@@ -4,8 +4,6 @@ package com.codepath.welldone.activity;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
@@ -46,25 +44,6 @@ public class DemoPushActivity extends Activity {
         this.genderMaleButton = (RadioButton) findViewById(R.id.gender_male_button);
         this.ageEditText = (EditText) findViewById(R.id.age_edit_text);
         this.genderRadioGroup = (RadioGroup) findViewById(R.id.gender_radio_group);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.demo_push, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     @Override
